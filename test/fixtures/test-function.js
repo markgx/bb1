2e06afbb4b95087e419884cba4b5b38cd4681212
Mark Guerra <markgx@gmail.com>
2016-06-12 01:33:22 -0700

    Implement registering/unregistering functions

---
+++ test/fixtures/test-function.js
@@ -0,0 +1,6 @@
+module.exports = {
+  name: 'testFunction',
+  fn: function() {
+    return 'test';
+  }
+};
--- test/test.js
+++ test/test.js
@@ -1,24 +1,43 @@
 const assert = require('chai').assert;
+const path = require('path');
 const BB1 = require('..');
 
 describe('BB1', function() {
-  describe('constructor', function() {
+  describe('#constructor', function() {
   });
 
   describe('#execute', function() {
   });
 
+  const someFunction = {
+    name: 'someFunction',
+    fn: function() { return 'hi'; }
+  };
+
   describe('#registerFunction', function() {
-    it('should add itself to the commands instance variable', function() {
+    it('should add the given function to the functions instance variable', function() {
       const bb1 = new BB1();
-
-      const someFunction = {
-        name: 'someFunction',
-        fn: function() { return 'hi'; }
-      };
-
       bb1.registerFunction(someFunction);
       assert.equal(someFunction, bb1.functions.someFunction);
+    });
+  });
+
+  describe('#registerFunctionFromFile', function() {
+    it('should register a function from a file given a valid path', function() {
+      const bb1 = new BB1();
+      const filepath = path.resolve(`${__dirname}/fixtures/test-function`);
+      const fn = require(filepath);
+      bb1.registerFunctionFromFile(filepath);
+      assert.equal(fn, bb1.functions[fn.name]);
+    });
+  });
+
+  describe('#unregisterFunction', function() {
+    it('should remove the named function from the functions instance variable', function() {
+      const bb1 = new BB1();
+      bb1.registerFunction(someFunction);
+      bb1.unregisterFunction(someFunction.name);
+      assert.equal(null, bb1.functions[someFunction.name]);
     });
   });
 });
