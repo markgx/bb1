4b19996f1b2e244f363a954558cde516f99797b2
Mark Guerra <markgx@gmail.com>
2016-06-12 22:27:58 -0700

    Add register specs

--- test/test.js
+++ test/test.js
@@ -3,6 +3,8 @@ const path = require('path');
 const BB1 = require('..');
 
 describe('BB1', function() {
+  const testFilepath = path.resolve(`${__dirname}/fixtures/test-function`);
+
   describe('#constructor', function() {
   });
 
@@ -20,15 +22,27 @@ describe('BB1', function() {
       bb1.registerFunction(someFunction);
       assert.equal(someFunction, bb1.functions.someFunction);
     });
+
+    it('should add the given function with the specified name', function() {
+      const bb1 = new BB1();
+      bb1.registerFunction(someFunction, 'anotherName');
+      assert.equal(someFunction, bb1.functions['anotherName']);
+    });
   });
 
   describe('#registerFunctionFromFile', function() {
     it('should register a function from a file given a valid path', function() {
       const bb1 = new BB1();
-      const filepath = path.resolve(`${__dirname}/fixtures/test-function`);
-      const fn = require(filepath);
-      bb1.registerFunctionFromFile(filepath);
+      const fn = require(testFilepath);
+      bb1.registerFunctionFromFile(testFilepath);
       assert.equal(fn, bb1.functions[fn.name]);
+    });
+
+    it('should register a function from a file with the specified name', function() {
+      const bb1 = new BB1();
+      const fn = require(testFilepath);
+      bb1.registerFunctionFromFile(testFilepath, 'anotherName');
+      assert.equal(fn, bb1.functions['anotherName']);
     });
   });
 
